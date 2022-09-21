59eae93774e23abcb4b41860732a3f0b52096d3b
Fedor Lisin <theo.lisin@gmail.com>
2022-09-21 14:57:34 +0300

    add setup.py and config

---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Recsys MADE project",
+    author="DreamTeam",
+    author_email="theo.lisin@gmail.com",
+    description="MADE Python project",
+    keywords="Python, MADE, recsys",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheoLisin/recsys_MADE",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    version="0.1.0",
+    classifiers=[
+        # see https://pypi.org/classifiers/
+        "Development Status :: 1 - Alpha",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "wemake-python-styleguide",
+            "mypy",
+            "black",
+        ],
+        "tests": [
+            "pytest",
+            "pytest-dotenv",
+        ],
+    },
+)
