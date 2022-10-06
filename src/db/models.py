c3bb49b1c839c268f3218b6b39158fd5102cfc8c
Fedor Lisin <theo.lisin@gmail.com>
2022-10-06 04:45:31 +0300

    new models

--- src/db/models.py
+++ src/db/models.py
@@ -1,8 +1,10 @@
 """Tables representations in SQLAlchemy."""
 import bcrypt
+
+from db.utils import Pows
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String, Index
-from sqlalchemy.types import Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Index, Boolean
+from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.orm import relationship
 
 
@@ -15,10 +17,13 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    login = Column(String(length=64))
-    pwdhash = Column(String(length=64))
+    login = Column(String(length=Pows.p6))
+    pwdhash = Column(String(length=Pows.p6))
     author = relationship("Author", back_populates="user", uselist=False)
-    __table_args__ = (Index("users_ind", id),)
+    __table_args__ = (
+        Index("users_ind", id),
+        {"mariadb_charset": "utf8mb4", "mariadb_collate": "utf8mb4_general_ci"},
+    )
 
     def set_pwd(self, password: str) -> None:
         bpass = bytes(password, "UTF-8")
@@ -29,8 +34,10 @@ class User(Base):
         bpass = bytes(password, "UTF-8")
         return bcrypt.checkpw(bpass, self.pwdhash)
 
+
 class ArticleAuthor(Base):
     """Article-Author association model."""
+
     __tablename__ = "article_author"
     id_article = Column(Integer, ForeignKey("articles.id"), primary_key=True)
     id_author = Column(Integer, ForeignKey("authors.id"), primary_key=True)
@@ -38,6 +45,7 @@ class ArticleAuthor(Base):
 
 class ArticleKeyword(Base):
     """Article-Keyword association model."""
+
     __tablename__ = "article_keyword"
     id_keyword = Column(Integer, ForeignKey("keywords.id"), primary_key=True)
     id_article = Column(Integer, ForeignKey("articles.id"), primary_key=True)
@@ -50,46 +58,57 @@ class Author(Base):
 
     id = Column(Integer, primary_key=True)
     id_user = Column(Integer, ForeignKey("users.id"))
-    old_id = Column(String(length=64))
-    avatar = Column(String(length=128))
-    sid = Column(String(length=256))
-    name = Column(String(length=128))
-    organisations = Column(String(length=1024))
-    orcid = Column(String(length=128))
-    position = Column(String(length=32))
-    email = Column(String(length=128))
-    bio = Column(String(length=4096))
-    homepage = Column(String(length=256))
+    old_id = Column(String(length=Pows.p6))
+    gid = Column(String(length=Pows.p5))
+    sid = Column(String(length=Pows.p6))
+    name = Column(String(length=Pows.p7))
+    organisation = Column(String(length=Pows.p11))
+    orgid = Column(String(length=Pows.p7))
+    orgs_count = Column(Integer)
+    email = Column(String(length=Pows.p7))
     user = relationship("User", back_populates="author")
     articles = relationship(
-        "Article", secondary="article_author", back_populates="authors",
+        "Article",
+        secondary="article_author",
+        back_populates="authors",
     )
-    __table_args__ = (Index("author_ind", id),)
+    __table_args__ = (
+        Index("author_ind", id),
+        {"mariadb_charset": "utf8mb4", "mariadb_collate": "utf8mb4_general_ci"},
+    )
+
 
 class Venue(Base):
     """Venue model."""
 
     __tablename__ = "venues"
     id = Column(Integer, primary_key=True)
-    old_id = Column(String(length=64))
-    raw_en = Column(String(length=256))
-    name = Column(String(length=256))
-    name_d = Column(String(length=256))
-    sid = Column(String(length=256))
-    publisher = Column(String(length=256))
-    issn = Column(String(length=16))
+    old_id = Column(String(length=Pows.p6))
+    raw_en = Column(String(length=Pows.p10))
+    name_d = Column(String(length=Pows.p8))
+    type = Column(String(length=Pows.p3))
     articles = relationship("Article", back_populates="venue")
-    __table_args__ = (Index("venue_ind", id),)
+    __table_args__ = (
+        Index("venue_ind", id),
+        {"mariadb_charset": "utf8mb4", "mariadb_collate": "utf8mb4_general_ci"},
+    )
 
 
 class Keyword(Base):
     """Tags/keywords model."""
+
     __tablename__ = "keywords"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128))
+    name = Column(String(length=Pows.p9))
     articles = relationship(
-        "Article", secondary="article_keyword", back_populates="keywords",
+        "Article",
+        secondary="article_keyword",
+        back_populates="keywords",
     )
+    __table_args__ = {
+        "mariadb_charset": "utf8mb4",
+        "mariadb_collate": "utf8mb4_general_ci",
+    }
 
 
 class Reference(Base):
@@ -106,20 +125,17 @@ class Article(Base):
     __tablename__ = "articles"
     id = Column(Integer, primary_key=True)
     id_venue = Column(Integer, ForeignKey("venues.id"))
-    old_id = Column(String(length=64))
-    title = Column(String(length=1024))
-    pub_year = Column(String(length=8))
+    old_id = Column(String(length=Pows.p6))
+    title = Column(String(length=Pows.p10))
+    year = Column(Integer)
     n_citation = Column(Integer)
-    page_start = Column(Integer)
-    page_end = Column(Integer)
-    volume = Column(String(length=32))
-    lang = Column(String(length=2))
-    issue = Column(String(length=64))
-    issn = Column(String(length=512))
-    isbn = Column(String(length=64))
-    doi = Column(String(length=256))
-    url_pdf = Column(String(length=512))
-    abstract = Column(Text)
+    article_len = Column(Integer)
+    lang = Column(String(length=Pows.p2))
+    issn = Column(String(length=Pows.p9))
+    isbn = Column(String(length=Pows.p6))
+    abstract = Column(LONGTEXT)
+    has_doi = Column(Boolean)
+    fos = Column(String(length=Pows.p13))
     venue = relationship("Venue", back_populates="articles")
 
     references = relationship(
@@ -130,10 +146,17 @@ class Article(Base):
         backref="id_where",
     )
 
-    authors = relationship("Author", secondary="article_author", back_populates="articles")
-
-    keywords = relationship(
-        "Keyword", secondary="article_keyword", back_populates="articles",
+    authors = relationship(
+        "Author", secondary="article_author", back_populates="articles"
     )
 
-    __table_args__ = (Index("article_ind", id),)
+    keywords = relationship(
+        "Keyword",
+        secondary="article_keyword",
+        back_populates="articles",
+    )
+
+    __table_args__ = (
+        Index("article_ind", id),
+        {"mariadb_charset": "utf8mb4", "mariadb_collate": "utf8mb4_general_ci"},
+    )
