2a032766aad8797eeb8a79408fa56d65fc9e0d96
Theo <theo.lisin@gmail.com>
2022-11-28 01:03:25 +0300

    added JDBC url

--- src/db/db_params.py
+++ src/db/db_params.py
@@ -12,11 +12,11 @@ DB_USER = environ.get("DB_USER")
 DB_PASSWORD = environ.get("DB_PASSWORD")
 DB_HOST = environ.get("DB_HOST")
 DB_NAME = environ.get("DB_NAME")
-DB_PORT = environ.get("DB_PORT") or "1234"
+DB_PORT = environ.get("DB_PORT") or "5432"
 
 
 SYNC_SQLALCHEMY_DATABASE_URL = (
-    "mariadb+mariadbconnector://{user}:{pasw}@{host}:{port}/{name}".format(
+    "postgresql+psycopg2://{user}:{pasw}@{host}:{port}/{name}".format(
         user=DB_USER,
         pasw=DB_PASSWORD,
         host=DB_HOST,
@@ -25,17 +25,13 @@ SYNC_SQLALCHEMY_DATABASE_URL = (
     )
 )
 
-MSQL_SQLALCHEMY_DATABASE_URL = (
-    "mariadb+pymysql://{user}:{pasw}@{host}:{port}/{name}".format(
-        user=DB_USER,
-        pasw=DB_PASSWORD,
-        host=DB_HOST,
-        name=DB_NAME,
-        port=DB_PORT,
-    )
+JDBC_SQLALCHEMY_DATABASE_URL = "jdbc:postgresql://{host}:{port}/{name}".format(
+    host=DB_HOST,
+    name=DB_NAME,
+    port=DB_PORT,
 )
 
-engine = create_engine(SYNC_SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SYNC_SQLALCHEMY_DATABASE_URL, echo=True)
 
 # тут ещё есть автокоммит, помимо expire_on_commit
 # стоит посмотреть надо ли оно вообще
