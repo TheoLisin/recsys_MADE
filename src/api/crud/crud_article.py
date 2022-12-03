9b235eba3806b656a2b1e8cfce5d5f0b5ec19581
Theo <theo.lisin@gmail.com>
2022-12-04 00:57:33 +0300

    added tdg filtering

--- src/api/crud/crud_article.py
+++ src/api/crud/crud_article.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import select
 
 from api.crud.crud_base import BaseFilter
 from api.core.api_config import PAGINATION_LIMIT
-from db.models import Article, Author, ArticleAuthor, Venue
+from db.models import Article, Author, ArticleAuthor, Venue, ArticleTag, Tag
 
 
 class YearFilter(BaseFilter):
@@ -37,9 +37,49 @@ class VenueFilter(BaseFilter):
         Returns:
             Query: _description_
         """
-        if len(self.filter_param) > 2:
-            return query.filter(Article.venue.name_d.contains(self.filter_param))
-        return query.filter(Article.venue.has(name_d=self.filter_param))
+        filter_join = (
+            select(
+                Venue.name_d.label("name"),
+                Article.id.label("id_article"),
+            )
+            .join(Venue)
+            .where(Venue.name_d.ilike(f"%{self.filter_param}%"))
+            .cte(name="filter_join")
+        )
+        return query.filter(filter_join.c.id_article == Article.id)
+
+
+class TagFilter(BaseFilter):
+    def __init__(self, filter_param: str):
+        """Filter by name of tag.
+
+        There will be a search for an exact match if the length is less than 2,
+        inclusion otherwise.
+
+        Args:
+            filter_param (str): _description_
+        """
+        super().__init__(filter_param)
+
+    def add_filter(self, query: Query) -> Query:
+        """Add filter by venue name to query.
+
+        Args:
+            query (Query): query to add filter.
+
+        Returns:
+            Query: _description_
+        """
+        filter_join = (
+            select(
+                Tag.tag.label("tag"),
+                ArticleTag.id_article.label("id_article"),
+            )
+            .join(Tag)
+            .where(Tag.tag.ilike(f"{self.filter_param}"))
+            .cte(name="filter_join")
+        )
+        return query.filter(filter_join.c.id_article == Article.id)
 
 
 class AuthorNameFilter(BaseFilter):
@@ -49,7 +89,8 @@ class AuthorNameFilter(BaseFilter):
     def add_filter(self, query: Query) -> Query:
         filter_join = (
             select(
-                Author.name.label("name"), ArticleAuthor.id_article.label("id_article"),
+                Author.name.label("name"),
+                ArticleAuthor.id_article.label("id_article"),
             )
             .join(Author)
             .where(Author.name.ilike(f"%{self.filter_param}%"))
