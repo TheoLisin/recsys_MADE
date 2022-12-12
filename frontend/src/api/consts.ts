a02a02047fe5f310ac69e7f4f55677f2601bb601
Theo <theo.lisin@gmail.com>
2022-12-12 03:40:28 +0100

    root prefix for proxy

--- frontend/src/api/consts.ts
+++ frontend/src/api/consts.ts
@@ -1,6 +1,6 @@
 import { range } from "../utils/range";
 
-export const PREFIX = "http://apihost:5555";
+export const PREFIX = "/api";
 
 export const FIRST_PAGE_INDEX = 1;
 export const PAGE_SIZE = 10;
